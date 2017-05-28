8e19619ddfaeac734f5e67b9d79c83db03fe05ac
zeddysoft <azeeztaiwo942@gmail.com>
2017-05-28 20:57:42 +0100

    Hindex solution

--- azeez_taiwo/H_Index.java
+++ azeez_taiwo/H_Index.java
@@ -6,7 +6,6 @@
 package big4prep;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,14 +16,47 @@ import java.util.Map;
  */
 public class H_Index {
 
+    Map<Integer, List<Integer>> notMoreThan = new HashMap<>();
+    Map<Integer, List<Integer>> atLeast = new HashMap<>();
+    int hIndex;
+
+    public void process(int i, int j, Map<Integer, List<Integer>> map) {
+        List<Integer> l = map.get(i);
+        if (l != null) {
+            l.add(j);
+            map.put(i, l);
+        } else {
+            l = new ArrayList<>();
+            l.add(j);
+            map.put(i, l);
+        }
+
+    }
+
     public int hIndex(int[] citations) {
 
-        Arrays.sort(citations);
+        for (int i = 1; i < citations.length; ++i) {
 
-        int hIndex = 0;
-        for (int i = 0; i < citations.length; i++) {
-            int min = Math.min(citations[i], citations.length - i);
-            hIndex = Math.max(hIndex, min);
+            for (int j = 0; j < citations.length; ++j) {
+                if (citations[j] <= i) {
+                    process(i, j, notMoreThan);
+                }
+
+                if (citations[j] >= i) {
+                    process(i, j, atLeast);
+                }
+            }
+        }
+
+        for (int i = citations.length - 1; i > 0; --i) {
+            int notMoreThanCount = notMoreThan.get(i) == null ? 0 : notMoreThan.get(i).size();
+
+            int atLeastCount = atLeast.get(i) == null ? 0 : atLeast.get(i).size();
+            if (notMoreThanCount == atLeastCount && notMoreThanCount == i) {
+                hIndex = i;
+                break;
+            }
+
         }
 
         return hIndex;
